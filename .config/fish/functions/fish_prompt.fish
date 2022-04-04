e2f82bf30588bd9647d41246701adcff25b8170d
Magnus Åsrud <magnusasrud92@gmail.com>
2022-04-04 11:42:25 +0200

    Cleanup in fish prompt.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,21 +1,23 @@
 function fish_prompt --description="Custom terminal prompt"
-    # set -l user (set_color blue) (whoami) (set_color normal)
-    # set -l machine (set_color magenta) (hostname) (set_color normal)
     set -l directory (set_color '#4DA5FF') (pwd | sed "s|^$HOME|~|") (set_color normal)
     set -l chevron (set_color '#A3C1D7') ' >'
-
-    # echo -ns $user '@' $machine ' ' $directory
 
     # If there is a SSH session...
     if test -n "$SSH_CLIENT"
       echo -ns (set_color '#FF748C') (whoami) '@' (hostname) ' (ssh)' (set_color normal) ' '
     end
 
+    # result: [current directory] >
     echo -ns [ $directory ] $chevron
 
+    # Changes the color of the git prompt if there are changes if there are
+    # unstaged changes.
     if test (git status --porcelain 2>/dev/null | wc -l) -eq 0
-        echo -s (set_color '#6DA08C') (__fish_git_prompt) (set_color normal) ' '
+        set -f git_status '#6DA08C'
     else
-        echo -s (set_color yellow) (__fish_git_prompt) (set_color normal) ' '
+        set -f git_status yellow
     end
+
+    # result: (branch)
+    echo -s (set_color $git_status) (__fish_git_prompt) (set_color normal) ' '
 end
