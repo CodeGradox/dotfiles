e6ca96895a9b2d232c90efb26eee778310cab288
Magnus Åsrud <magnusasrud92@gmail.com>
2022-04-12 15:08:26 +0200

    Better colors for diffs in vim-fugitive.

--- .config/nvim/lua/gradox/colorscheme.lua
+++ .config/nvim/lua/gradox/colorscheme.lua
@@ -5,3 +5,8 @@ vim.cmd [[colorscheme onehalf-lush]]
 
 -- Disable nvim background
 vim.cmd [[hi Normal guibg=NONE]]
+
+-- Colors for diff in vim-fugitive. Matches the onehalf-lush theme.
+vim.cmd [[hi diffAdded guifg=#50A14F guibg=NONE]]
+vim.cmd [[hi diffRemoved guifg=#E45649 guibg=NONE]]
+vim.cmd [[hi diffChanged guifg=#C18401 guibg=NONE]]
