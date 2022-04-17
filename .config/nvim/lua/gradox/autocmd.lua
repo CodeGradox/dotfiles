a1922aaf34c90b3c80af9c6e70e954c67075fe09
Magnus Åsrud <magnusasrud92@gmail.com>
2022-04-17 13:08:17 +0200

    Use lua to set autocommands in neovim 0.7.

--- .config/nvim/lua/gradox/autocmd.lua
+++ .config/nvim/lua/gradox/autocmd.lua
@@ -1,16 +1,21 @@
 -- Autocommands
--- It does not look like there is a lua function for setting autommands, so we
--- use `vim.cmd`.
 
 -- Trim trailing whitespace on save.
-vim.cmd [[autocmd BufWritePre * %s/\s\+$//e]]
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = "%s/\\s\\+$//e",
+})
 
 -- Overwrite global rules for certain filetypes.
 -- Rust
-vim.cmd [[autocmd filetype rs setlocal tabstop=4 shiftwidth=4 softtabstop=4]]
--- Caracal (ruby)
-vim.cmd [[autocmd BufNewFile,BufRead *.caracal set syntax=ruby]]
--- Thor (ruby)
-vim.cmd [[autocmd BufNewFile,BufRead *.thor set syntax=ruby]]
--- Tell CSV rainbow we like to separtate columns with commas.
-vim.cmd [[autocmd BufNewFile,BufRead *.csv set filetype=csv]]
+-- vim.cmd [[autocmd filetype rs setlocal tabstop=4 shiftwidth=4 softtabstop=4]]
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "rs" },
+  command = "setlocal tabstop=4 shiftwidth=4 softtabstop=4",
+})
+
+-- Caracal and thor (ruby)
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufEnter" }, {
+  pattern = { "*.thor", "*.caracal" },
+  command = "set syntax=ruby",
+})
