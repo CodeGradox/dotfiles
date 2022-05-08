a225921d07c2277c03194cf3b79be47a215fd260
Magnus Åsrud <magnusasrud92@gmail.com>
2022-05-08 21:38:14 +0200

    Highlight unwanted spaces in bright red

--- .config/nvim/lua/gradox/colorscheme.lua
+++ .config/nvim/lua/gradox/colorscheme.lua
@@ -10,3 +10,6 @@ vim.cmd [[hi Normal guibg=NONE]]
 vim.cmd [[hi diffAdded guifg=#50A14F guibg=NONE]]
 vim.cmd [[hi diffRemoved guifg=#E45649 guibg=NONE]]
 vim.cmd [[hi diffChanged guifg=#C18401 guibg=NONE]]
+
+-- Highlight unwanted spaces in bright red.
+vim.cmd [[hi Whitespace guifg=#FFFFFF guibg=#E45649]]
